ca9c166b3f03fea7c6d6a61013e4941b2d43947e
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-17 17:23:18 -0300

    Configurando banco de dados

---
+++ backend/src/index.js
@@ -0,0 +1,31 @@
+const express = require ('express');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+// Rota - Recurso
+
+//Métodos HTTP =>
+//GET: Buscar/Listar uma informação do backend;
+//POST: Criar uma informação no backend;
+//PUT: Alterar uma informação no backend;
+//DELETE: Deletar uma informação no backend;
+
+//Tipos de parâmetros =>
+//Query Params: './users?name=Diego&idade=22' Parâmetros nomeados enviados na rota após "?" (Filtros, paginação);
+//Route Params: './users/:id' Parâmetros utilizados para identificar recursos, buscar dados de um único usuário;
+//Request Body: Corpo da requisição, utilizado para criar ou alterar recursos.
+
+//Request: Guarda todos os dados que vem através da requisição do usuário // 'request.example'
+//Response: Retorna uma resposta para o usuário
+
+//SQL: MySQL, SQLite, PostgreSQL, Oracle, etc.
+//NoSQL: MongoDB, CouchDB, etc.
+
+//Driver: SELECT * FROM users
+//Query Builder: table('users').select('*').where()
