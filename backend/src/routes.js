7062948cf8c8b293940b62240d9155895ab8b815
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-17 20:24:18 -0300

    Login

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
 const OngController = require('./controllers/OngController');
-const ProfileController = require('./controllers/ProfileController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
