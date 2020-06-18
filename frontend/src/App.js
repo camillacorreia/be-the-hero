6263f3575a64ce1ff021db70eca36ea653195427
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-17 23:00:36 -0300

    Página de Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,11 @@
-import React, { useState } from 'react';
-import Header from './Header';
+import React from 'react';
+import Logon from './pages/Logon';
+import './global.css';
 
 function App() {
-  // Array [valor, funçãoDeAtualização]
-  const [ counter, setCounter] = useState (0);
-
-  function increment(){
-    setCounter(counter + 1);
-  }
-
   return (
-    <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
-    </div>
+    <Logon />
   );
-
 }
 
 export default App;
