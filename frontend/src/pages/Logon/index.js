50d96122003c8e3c799da9dda929f736e667a2e3
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-18 00:13:50 -0300

    Fazendo login

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -2,30 +2,58 @@ import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
+import api from '../../services/api';
+
 import './styles.css';
 
 import logoImg from '../../assets/logo.svg';
-import heroesImg from '../../assets/heroes.png';
+import heroesImg from '../../assets/heroes.png'
 
 export default function Logon() {
-    return (
-      <div className="logon-container">
-        <section className="form">
-          <img src={logoImg} alt="Be The Hero" />
+  const [id, setId] = useState('');
+  const history = useHistory();
 
-          <form>
-            <h1>Faça seu logon</h1>
-            <input placeHolder="Sua ID" />
-            <button className="button" type="submit">Entrar</button>
+  async function handleLogin(e) {
+    e.preventDefault();
 
-            <Link className="back-link" to="/register">
-              <FiLogIn size={16} color="#E02041"/>
-              Não tenho cadastro
+    try {
+      const response = await api.post('sessions', { id });
+
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+
+      history.push('/profile');
+    } catch (err) {
+      alert('Falha no login, tente novamente.');
+    }
+  }
+
+  return (
+    
+    <div className="logon-container">
+      <section className="form">
+        <img src={logoImg} alt="Be The Hero" />
+
+        <form onSubmit={handleLogin}>
+          <h1>Faça seu logon</h1>
+
+          <input
+            placeHolder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
+
+          <button className="button" type="submit">Entrar</button>
+
+          <Link className="back-link" to="/register">
+            <FiLogIn size={16} color="#E02041"/>
+            Não tenho cadastro
             </ Link>
-          </form>
-        </section>
+        </form>
 
-        <img src={heroesImg} alt="Heroes" />
-      </div>
-    );
-} 
+      </section>
+
+      <img src={heroesImg} alt="Heroes" />
+    </div>
+  );
+}
