017b039779420cfcb483320ef554637a13d7a795
Camilla Correia <50275354+camillacorreia@users.noreply.github.com>
2020-06-19 18:56:40 -0300

    Adicionando testes
    npm install jest
    npx  jest --init
    npm test -D
    
    Configurando BANCO DE TESTES
    npm install cross-env
    npm install supertest -D
    
    Deploy NodeJS: heroku
    Deploy ReactJS: netlify

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,27 +1,27 @@
-const cryto = require('crypto');
-const connection = require('../database/connection');
+const generateUniqueId = require('../utils/generateUniqueId')
+const connection  = require('../database/connection')
 
 module.exports = {
-    async index(request, response) {
-        const ongs = await connection('ongs').select('*');
+  async index(request, response) {
+  const ongs = await connection('ongs').select('*')
 
-        return response.json(ongs);
-    },
+  return response.json(ongs)
+ },
 
-    async create(request, response) {
-        const {name, email, whatsapp, city, uf } = request.body;
+  async create(request, response) {
+    const { name, email, whatsapp, city, uf } = request.body
 
-        const id = cryto.randomBytes(4).toString('HEX');
+    const id = generateUniqueId()
 
-        await connection('ongs').insert({
-            id,
-            name,
-            email,
-            whatsapp,
-            city,
-            uf,
-        })
+    await connection('ongs').insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf,
+    })
 
-        return response.json({ id });
-    }
+    return response.json({ id })
+  }
 }; 
